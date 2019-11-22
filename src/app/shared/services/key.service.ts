0493dced3dcefe73d0c9f7db0065a2a4a6491769
Armin Kunkel <armin.kunkel@hs-karlsruhe.de>
2019-11-22 12:14:42 +0100

    Change router URL, fix linter issues in key.service, shorten deploy base ref

--- src/app/shared/services/key.service.ts
+++ src/app/shared/services/key.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { ConfigService } from "./config.service";
+import { Injectable } from '@angular/core';
+import { ConfigService } from './config.service';
 
 @Injectable({
     providedIn: 'root'
@@ -7,13 +7,14 @@ import { ConfigService } from "./config.service";
 export class CryptoModule {
 
     private secret_key = {}; // the AES secret key object
-    session_identifier = ""; // session identifier associated with the secret key
+    session_identifier = ''; // session identifier associated with the secret key
     private encoder = new TextEncoder(); // always utf-8
 
     constructor() { // call this function after the page has loaded
-        let key_service_api = ConfigService.getApiEndpoint('KEY_SERVICE_URL');
-        if (!("TextEncoder" in window))
-            alert("Sorry, this browser does not support TextEncoder...");
+        const key_service_api = ConfigService.getApiEndpoint('KEY_SERVICE_URL');
+        if (!('TextEncoder' in window)) {
+          alert('Sorry, this browser does not support TextEncoder...');
+        }
         // $.get("./secret.aes", cryptoMod.initCryptoKey); // use key from local sever instead of openwhisk action
         $.get(
             key_service_api,
@@ -22,48 +23,48 @@ export class CryptoModule {
         // $("#encrypt").on("click", () => demoPageMod.sendCryptoMsg());
     }
 
+    static buf2hex(buffer) { // buffer is an ArrayBuffer
+        return Array.prototype.map.call(
+            new Uint8Array(buffer),
+                x => ('00' + x.toString(16)).slice(-2)
+         ).join('');
+    }
+
     private hex2buf(hex) { // hex is a hex string
-        var view = new Uint8Array(hex.length / 2);
-        for (var i = 0; i < hex.length; i += 2) {
+        const view = new Uint8Array(hex.length / 2);
+        for (let i = 0; i < hex.length; i += 2) {
             view[i / 2] = parseInt(hex.substring(i, i + 2), 16);
         }
         return view.buffer;
     }
 
-    static buf2hex(buffer) { // buffer is an ArrayBuffer
-        return Array.prototype.map.call(
-            new Uint8Array(buffer),
-            x => ('00' + x.toString(16)).slice(-2)
-        ).join('');
-    }
-
     private importSecretAesKey(keyData, session_id) {
         this.session_identifier = session_id;
         return window.crypto.subtle.importKey(
-            "raw", //can be "jwk" or "raw"
+            'raw', // can be "jwk" or "raw"
             keyData, // "raw" would be an ArrayBuffer
-            "AES-CBC",
-            false, //whether the key is extractable (i.e. can be used in exportKey)
-            ["encrypt", "decrypt"] //can "encrypt", "decrypt", "wrapKey", or "unwrapKey"
+            'AES-CBC',
+            false, // whether the key is extractable (i.e. can be used in exportKey)
+            ['encrypt', 'decrypt'] // can "encrypt", "decrypt", "wrapKey", or "unwrapKey"
         );
     }
 
     private encryptPlaintextWithAes(data, aesKey) {
-        var ivec = window.crypto.getRandomValues(new Uint8Array(16));
+        const ivec = window.crypto.getRandomValues(new Uint8Array(16));
         return window.crypto.subtle.encrypt({
-            name: "AES-CBC",
-            //Don't re-use initialization vectors!
-            //Always generate a new iv every time your encrypt!
+            name: 'AES-CBC',
+            // Don't re-use initialization vectors!
+            // Always generate a new iv every time your encrypt!
             iv: ivec,
         },
-            aesKey, //from generateKey or importKey above
-            data //ArrayBuffer of data you want to encrypt
+            aesKey, // from generateKey or importKey above
+            data // ArrayBuffer of data you want to encrypt
         )
         .then(encrypted => {
             return {
-                "iv": CryptoModule.buf2hex(ivec),
-                "encrypted": CryptoModule.buf2hex(new Uint8Array(encrypted)),
-                "sid": this.session_identifier
+                'iv': CryptoModule.buf2hex(ivec),
+                'encrypted': CryptoModule.buf2hex(new Uint8Array(encrypted)),
+                'sid': this.session_identifier
             };
         });
     }
@@ -74,8 +75,8 @@ export class CryptoModule {
     }
 
     public createEncryptedJsonMessage(plaintext) {
-        let data = this.encoder.encode(plaintext);
+        const data = this.encoder.encode(plaintext);
         return this.encryptPlaintextWithAes(data, this.secret_key)
             .then(msg => Promise.resolve(JSON.stringify(msg)));
     }
-};
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiEndpoints: {
-    CONVERSATION_API_URL: 'https://a5c3441f.us-south.apiconnect.appdomain.cloud/iwibotapi/router',
+    CONVERSATION_API_URL: 'https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/f5a899b31e8234cfabc39f0fd4e3774c59148dfc796d0f49fd774e58de9ab11f/iwibot/router', // tslint:disable-line
     HSKA_STUDENT_INFO_URL: 'https://www.iwi.hs-karlsruhe.de/iwii/REST/credential/v2/info',
     BULLETIN_BOARD_NEWS_URL: 'https://www.iwi.hs-karlsruhe.de/iwii/REST/newsbulletinboard',
     HSKA_LIB_BORROWED_BOOKS_URL: 'https://www.iwi.hs-karlsruhe.de/iwii/REST/library/borrowedbooks',
